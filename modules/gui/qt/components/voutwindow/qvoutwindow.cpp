743c942f5b303bc090d50203f10b15f18c070288
Pierre Lamot <pierre@videolabs.io>
2019-01-17 17:34:16 +0100

    qt: provide base classes to render video output within the interface
      VideoSurface provide a QQuickItem which can be embed within a QML scene to
      represent the video node.
    
      QVoutWindow implement a window where the video will be renderered
    
      VideoSurfaceProvider is the actual implementation of the VideoSurface which will
      be selected at runtime. it will act as a the bridge between the QML Videosurface
      and the QVoutWindow.

---
+++ modules/gui/qt/components/voutwindow/qvoutwindow.cpp
@@ -0,0 +1,94 @@
+#include "qvoutwindow.hpp"
+#include "util/customwidgets.hpp" //for qtEventToVLCKey
+
+QVoutWindow::QVoutWindow(QObject* parent)
+    : QObject(parent)
+{
+}
+
+QVoutWindow::~QVoutWindow()
+{
+}
+
+void QVoutWindow::setupVoutWindow(vout_window_t* window)
+{
+    QMutexLocker lock(&m_voutlock);
+    m_voutWindow = window;
+    m_hasVideo = false;
+}
+
+void QVoutWindow::enableVideo(unsigned /*width*/, unsigned /*height*/, bool /*fullscreen*/)
+{
+    QMutexLocker lock(&m_voutlock);
+    if (m_voutWindow) {
+        m_hasVideo = true;
+        if (m_surfaceSize.isValid())
+            vout_window_ReportSize(m_voutWindow, m_surfaceSize.width(), m_surfaceSize.height());
+    }
+}
+
+void QVoutWindow::disableVideo()
+{
+    QMutexLocker lock(&m_voutlock);
+    if (m_voutWindow)
+        m_hasVideo = false;
+}
+
+void QVoutWindow::windowClosed()
+{
+    QMutexLocker lock(&m_voutlock);
+    if (m_voutWindow)
+        vout_window_ReportClose(m_voutWindow);
+}
+
+void QVoutWindow::onMousePressed(int vlcButton)
+{
+    QMutexLocker lock(&m_voutlock);
+    if (m_hasVideo)
+        vout_window_ReportMousePressed(m_voutWindow, vlcButton);
+}
+
+void QVoutWindow::onMouseReleased(int vlcButton)
+{
+    QMutexLocker lock(&m_voutlock);
+    if (m_hasVideo)
+        vout_window_ReportMouseReleased(m_voutWindow, vlcButton);
+}
+
+void QVoutWindow::onMouseDoubleClick(int vlcButton)
+{
+    QMutexLocker lock(&m_voutlock);
+    if (m_hasVideo)
+        vout_window_ReportMouseDoubleClick(m_voutWindow, vlcButton);
+}
+
+void QVoutWindow::onMouseMoved(float x, float y)
+{
+    QMutexLocker lock(&m_voutlock);
+    if (m_hasVideo)
+        vout_window_ReportMouseMoved(m_voutWindow, x, y);
+}
+
+void QVoutWindow::onMouseWheeled(QWheelEvent* event)
+{
+    int vlckey = qtWheelEventToVLCKey(event);
+    QMutexLocker lock(&m_voutlock);
+    if (m_hasVideo)
+        vout_window_ReportKeyPress(m_voutWindow, vlckey);
+}
+
+void QVoutWindow::onKeyPressed(QKeyEvent* event)
+{
+    int vlckey = qtEventToVLCKey(event);
+    QMutexLocker lock(&m_voutlock);
+    if (m_hasVideo)
+        vout_window_ReportKeyPress(m_voutWindow, vlckey);
+}
+
+void QVoutWindow::onSurfaceSizeChanged(QSizeF size)
+{
+    m_surfaceSize = size;
+    QMutexLocker lock(&m_voutlock);
+    if (m_hasVideo)
+        vout_window_ReportSize(m_voutWindow, size.width(), size.height());
+}
